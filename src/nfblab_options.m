ca6c64c594c78b6b6b12c312d895cc1fb2cc54ae
Arnaud Delorme <arnodelorme@gmail.com>
2017-07-05 18:20:51 +0200

    nflab_option.m, option file

---
+++ src/nfblab_options.m
@@ -0,0 +1,8 @@
+
+nchans = 64; % number of channels
+ntrials = 8; % number of trials per day
+ndays   = 8; % number of days of training
+
+lsltype = 'EEG'; % put to empty if you cannot connect to your system
+lslname = ''; % this is the name of the stream that shows in Lab Recorder
+              % if empty, it will only use the type above
