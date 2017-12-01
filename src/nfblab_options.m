7b8f71aba33a7320295ed23aae7b2edb3a8135cb
Arnaud Delorme <arnodelorme@gmail.com>
2017-11-30 17:36:44 -0800

    adding comments

--- src/nfblab_options.m
+++ src/nfblab_options.m
@@ -1,8 +1,10 @@
 
-nchans = 64; % number of channels
 ntrials = 8; % number of trials per day
 ndays   = 8; % number of days of training
 
 lsltype = 'EEG'; % put to empty if you cannot connect to your system
 lslname = ''; % this is the name of the stream that shows in Lab Recorder
               % if empty, it will only use the type above
+              % USE lsl_resolve_byprop(lib, 'type', lsltype, 'name',
+              % lslname) to connect to the stream. If you cannot connect
+              % nfblab won't be able to connect either.
