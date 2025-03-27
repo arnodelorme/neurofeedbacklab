b73d0d855340efe40f2b4a152efdc5cda9037cd6
Arnaud Delorme <arnodelorme@gmail.com>
2025-03-27 13:41:24 -0700

    adding mean and mean2 as output

--- src/nfblab_loadasr.m
+++ src/nfblab_loadasr.m
@@ -1,7 +1,7 @@
 % nfblab_process support function to load ASR files
 % stored during the baseline period
 
-function [stateAsr, dynRange, icaWeights, icaWinv, icaRmInd, badChans] = nfblab_loadasr(fileNameAsr) % modify options ASR and ICA
+function [stateAsr, dynRange, icaWeights, icaWinv, icaRmInd, badChans, zmean, zmean2] = nfblab_loadasr(fileNameAsr) % modify options ASR and ICA
 
 fprintf('Loading baseline ASR file %s...\n', fileNameAsr);
 stateFile = load('-mat', fileNameAsr);
@@ -14,4 +14,12 @@ if isfield(stateFile, 'badChans')
     badChans = stateFile.badChans;
 else
     badChans = [];
+end
+
+if isfield(stateFile, 'zmean')
+    zmean  = stateFile.zmean;
+    zmean2 = stateFile.zmean2;
+else
+    zmean  = 0;
+    zmean2 = 0;
 end
